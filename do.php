8d3f668c7936c9120c1d93f727f931454b2ba055
anithagunji <141129797+anithagunji@users.noreply.github.com>
2023-08-04 11:54:23 +0530

    Add files via upload

---
+++ do.php
@@ -0,0 +1,7 @@
+<?php
+$a=2;
+do{
+              echo"anitha";
+}while($b==1);
+
+?>
---
+++ for.php
@@ -0,0 +1,8 @@
+<?php
+$sum=0
+for($x=1;$x<=30;$x++)
+{
+              sum+=$x;
+}
+echo "the sum of the numbers 1 to 30 is $sum"."\n";
+?>
